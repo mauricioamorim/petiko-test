dfc97ee316ce3495c33efea3a6321c40cd0a3fa6
Mauricio <mauricioamorim.contato@gmail.com>
2021-06-13 14:20:57 -0300

    Fineshed Front-End

---
+++ src/resources/views/shopping/head.blade.php
@@ -0,0 +1,35 @@
+<meta name="author" content=">BOX.Petiko">
+<meta name="description" content="Exame técnico Processo Seletivo – Desenvolvedor - Mauricio de Amorim Alexandre da Silva">
+<meta property="og:title" content="Processo Seletivo – Desenvolvedor - Mauricio de Amorim Alexandre da Silva"/>
+<meta property="og:type" content="Exame técnico"/>
+<meta property="og:image" content="http://box.petiko.com.br/images/face-york.png"/>
+<meta property="og:image:type" content="image/jpeg">
+<meta property="og:url" content="http://box.petiko.com.br"/>
+<meta property="og:description" content="Exame técnico para vaga de desenvolvedor"/>
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<!--- FONTS -->
+<!-- Font Awesome 5.0 -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<!-- Google -->
+<link href='https://fonts.googleapis.com/css?family=Amatic+SC' rel='stylesheet' type='text/css'>
+
+<!-- Custom styles for this template -->
+
+<!-- Icons -->
+<link rel="apple-touch-icon" sizes="180x180" href="https://box.petiko.com.br/icons/apple-icon-180x180.png?v=2">
+<link rel="icon" type="image/png" sizes="32x32" href="https://box.petiko.com.br/icons/favicon-32x32.png?v=2">
+<link rel="icon" type="image/png" sizes="192x192" href="https://box.petiko.com.br/icons/android-icon-192x192.png?v=2">
+<link rel="icon" type="image/png" sizes="16x16" href="https://box.petiko.com.br/icons/favicon-16x16.png?v=2">
+<link rel="manifest" href="https://box.petiko.com.br/icons/manifest.json?v=2">
+<link rel="mask-icon" href="https://box.petiko.com.br/icons/safari-pinned-tab.svg?v=2" color="#5ac2c5">
+<link rel="shortcut icon" href="https://box.petiko.com.br/icons/favicon.ico?v=2">
+<meta name="apple-mobile-web-app-title" content="BOX.Petiko">
+<meta name="application-name" content="BOX.Petiko">
+<meta name="msapplication-TileColor" content="#0097D7">
+<meta name="msapplication-TileImage" content="https://box.petiko.com.br/icons/mstile-144x144.png?v=2">
+<meta name="msapplication-config" content="https://box.petiko.com.br/icons/browserconfig.xml?v=2">
+<meta name="theme-color" content="#0097D7">
+<!-- Page hiding snippet (recommended)  -->
+<title>BOX.Petiko | Exame técnico</title>
