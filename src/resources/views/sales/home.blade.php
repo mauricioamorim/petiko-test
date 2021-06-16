bd5a2e2d35cebcff5375f720b8caea9a554bbad2
Mauricio <mauricioamorim.contato@gmail.com>
2021-06-15 21:30:26 -0300

    Home ornament

--- src/resources/views/sales/home.blade.php
+++ src/resources/views/sales/home.blade.php
@@ -46,6 +46,6 @@
 </p>
 
 <div class="text-center py-5">
-    <a href="{{ route('sales.create') }}"> <button class="btn btn-info rounded-pill btn-lg mr-3">Finalizar Pedido</button></a>
-    <a href="{{ route('sales.list') }}"><button class="btn btn-warning rounded-pill btn-lg mr-3">Listar Pedidos</button></a>
+    <a href="{{ route('sales.create') }}"> <button class="btn btn-success rounded-pill btn-lg mr-3">Testar Cadastro de Pedido</button></a>
+    <a href="{{ route('sales.list') }}"><button class="btn btn-warning rounded-pill btn-lg mr-3">Listar Pedidos Cadastrados</button></a>
 </div>
